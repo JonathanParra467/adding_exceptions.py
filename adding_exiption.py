bb72ac76e345cc7092627a1de1857c5399a26870
JonathanParra467 <jparra1786@students.mchenry.edu>
2024-10-03 18:48:30 -0500

    Add files via upload

---
+++ adding_exiption.py
@@ -0,0 +1,7 @@
+def square_number():
+    number = input("Enter a number to square: ")
+    squared_number = int(number) ** 2
+    print(f"The square of {number} is {squared_number}.")
+
+
+square_number()
